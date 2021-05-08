d6d3708d4957194af5c398a7322e14d15a8591fc
Pethum-Jeewantha <pethumjeewantha1@gmail.com>
2021-05-08 09:54:29 +0530

    remove(int index) method

--- src/Demo.java
+++ src/Demo.java
@@ -3,10 +3,17 @@ import ds.SinglyLinkedList;
 public class Demo {
     public static void main(String[] args) {
         SinglyLinkedList s1= new SinglyLinkedList();
-        s1.add(100);
-        s1.add(200);
-        s1.add(300);
+        s1.add(10);
+        s1.add(20);
+        s1.add(30);
         s1.print();
         System.out.println(s1.size());
+        s1.add(40);
+        s1.add(50);
+        s1.add(60);
+        s1.add(70);
+        s1.print();
+        s1.remove(2);
+        s1.print();
     }
 }
--- src/ds/SinglyLinkedList.java
+++ src/ds/SinglyLinkedList.java
@@ -1,25 +1,42 @@
 package ds;
 
 public class SinglyLinkedList {
-    Node top;
+    Node front;
 
     public void add(int data){
         Node n1 = new Node(data);
-        n1.next = top;
-        top = n1;
+        if(front==null){
+            front = n1;
+        }else{
+            Node lastNode = front;
+            while (lastNode.next!=null){
+                lastNode = lastNode.next;
+            }
+            lastNode.next = n1;
+        }
     }
     public void add(int index, int data){
 
     }
     public void remove(int index){
-
+        Node temp = front;
+        for (int i = 0; i < index-1; i++) {
+            temp = temp.next;
+        }
+        System.out.println(temp.data);
+        Node temp1 = front;
+        for (int i = 0; i < index; i++) {
+            temp1 = temp1.next;
+        }
+        temp.next = temp1.next;
+        System.out.println(temp1.data);
     }
     public int get(int index){
         return 0;
     }
     public void print(){
         System.out.print("[");
-        Node temp = top;
+        Node temp = front;
         while(temp!=null){
             System.out.print(temp.data+", ");
             temp = temp.next;
@@ -27,11 +44,11 @@ public class SinglyLinkedList {
         System.out.println("\b\b]");
     }
     public void clear(){
-        top = null;
+        front = null;
     }
     public int size(){
         int count = 0;
-        Node temp = top;
+        Node temp = front;
         while (temp!=null){
             count++;
             temp = temp.next;
@@ -42,13 +59,13 @@ public class SinglyLinkedList {
         return false;
     }
     public boolean isEmpty(){
-        return top==null;
+        return front==null;
     }
 
     private class Node{
         private int data;
         private Node next;
-        public Node(int data) {
+        private Node(int data) {
             this.data = data;
         }
     }
